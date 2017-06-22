4f648e2e0f7714cf02200b92effe571e4c9f639e
Ekventor <zzespoozz@gmail.com>
2017-06-22 11:29:31 +0300

    +Add WeakReference example

---
+++ Patterns/WeakReferenceTest.cs
@@ -0,0 +1,23 @@
+﻿namespace Patterns
+{
+    using System;
+    using System.Threading;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class WeakReferenceTest
+    {
+        [Test]
+        public void TestWeakReference()
+        {
+            var reference = new WeakReference(new object());
+
+            Assert.IsFalse(reference.Target == null);
+
+            GC.Collect();
+            Thread.Sleep(1000); // Чтобы сборщик мусора точно успел отработать
+
+            Assert.IsTrue(reference.Target == null);
+        }
+    }
+}
