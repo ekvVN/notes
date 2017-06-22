bfc85a4cecdaf34dae48e9791684906f1322e90e
Ekventor <zzespoozz@gmail.com>
2017-06-22 11:19:20 +0300

    +Add Disposable pattern class

---
+++ Patterns/Disposable.cs
@@ -0,0 +1,34 @@
+﻿namespace Patterns
+{
+    using System;
+
+    public class Disposable : IDisposable
+    {
+        private bool _disposed = false;
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposeManaged)
+        {
+            if (!_disposed)
+            {
+                if (disposeManaged)  
+                {
+                    // Free managed objects
+                }
+
+                // Free unmanaged objects
+                _disposed = true;
+            }
+        }
+
+        ~Disposable()
+        {
+            Dispose(false);
+        }
+    }
+}
